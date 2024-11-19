d8e78b27035d35b1b2cb2a25d0ed18d62bbb9d9f
Lucas <developer.lucas@pm.me>
2024-11-19 12:43:23 -0500

    refactor: rename ToDoItem content prop to text

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,10 +1,10 @@
 "use client";
 import Checkbox from "./checkbox";
-export function ToDoItem({ content }: { content: string }) {
+export function ToDoItem({ text }: { text: string }) {
   return (
     <div className="bg-gray-400 w-48 h-9 items-center rounded-2xl flex px-2 py-1 transition hover:brightness-110 border border-gray-400">
       <Checkbox />
-      <p className="pl-3">{content}</p>
+      <p className="pl-3">{text}</p>
     </div>
   );
 }
