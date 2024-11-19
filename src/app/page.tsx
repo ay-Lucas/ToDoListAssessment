af43bc219b77a76a27561fb24245d52815bcd8d8
Lucas <developer.lucas@pm.me>
2024-11-18 22:54:58 -0500

    feat: add heading and list of items to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
+import { ToDoItem } from "@/components/todo-item";
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <div></div>
-    </div>
+    <main>
+      <div className="grid items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <div>
+          <h1 className="text-3xl pb-10 font-bold">ToDo List</h1>
+          <div className="space-y-3">
+            {Array.from({ length: 5 }).map((_, index) => (
+              <div key={index}>
+                <ToDoItem content={`${index}`} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
