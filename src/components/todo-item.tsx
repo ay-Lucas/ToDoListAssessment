7c927372f2935f915e6d5bb90e44bb629d224b81
Lucas <developer.lucas@pm.me>
2024-11-19 19:12:16 -0500

    feat: add x to ToDoItem to delete it, not yet functional

--- src/components/todo-item.tsx
+++ src/components/todo-item.tsx
@@ -1,10 +1,16 @@
 "use client";
+import { LucideX } from "lucide-react";
 import Checkbox from "./checkbox";
 export function ToDoItem({ text }: { text: string }) {
+  const handleChange = () => {};
+
   return (
-    <div className="bg-gray-400 w-48 h-9 items-center rounded-2xl flex px-2 py-1 transition hover:brightness-110 border border-gray-400">
-      <Checkbox />
-      <p className="pl-3">{text}</p>
+    <div className="bg-gray-400 min-w-72 min-h-9 items-center justify-between rounded-2xl flex px-2 py-1 transition hover:brightness-110 border border-gray-400">
+      <Checkbox onChange={handleChange} />
+      <p className="pl-3 text-left w-full">{text}</p>
+      <button className="hover:text-green-400 text-gray-600 transition duration-200">
+        <LucideX />
+      </button>
     </div>
   );
 }
