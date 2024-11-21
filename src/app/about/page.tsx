61b33a4dbd0586716f3fb0f07864848bbe4dc59e
Lucas <developer.lucas@pm.me>
2024-11-20 19:54:09 -0500

    feat: add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="grid justify-center items-center min-h-screen font-[family-name:var(--font-geist-sans)] ">
+      <div className="container space-y-7 h-2/3">
+        <h1 className="text-3xl font-bold">About</h1>
+      </div>
+    </div>
+  );
+}
